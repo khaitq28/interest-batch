d2605383e66a61484bb6a7e1267ccb67fe1a58d2
TRAN Quang Khai <canhcung88@gmail.com>
2026-03-28 23:57:23 +0100

    lást

--- src/main/java/com/fintech/batch/batch/InterestProcessor.java
+++ src/main/java/com/fintech/batch/batch/InterestProcessor.java
@@ -1,8 +1,6 @@
 package com.fintech.batch.batch;
 
 import com.fintech.batch.entity.Account;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +11,6 @@ import java.time.LocalDateTime;
 @Component
 public class InterestProcessor implements ItemProcessor<Account, Account> {
 
-    private static final Logger log = LoggerFactory.getLogger(InterestProcessor.class);
     private static final BigDecimal DAYS_IN_YEAR = new BigDecimal("365");
 
     @Override
